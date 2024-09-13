1d8903fbc2b453bcc34b8b39b128632726c9bea9
Gianni Carafa <gianni.carafa@srf.ch>
2024-09-13 04:58:15 +0200

    rename buildpacks

--- sidebars.js
+++ sidebars.js
@@ -73,7 +73,7 @@ const sidebars = {
             'Usermanual/templates/php',
           ],
         },
-        'Usermanual/buildpacks',
+        'Usermanual/runpacks',
       ],
     },
     'screenshots',
