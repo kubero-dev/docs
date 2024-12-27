48e73d276865a760163792377e92c185fb6a67e9
Gianni Carafa <gianni.carafa@srf.ch>
2024-12-27 22:17:50 +0100

    add feature list

--- src/components/HomepageFeatures/features.tsx
+++ src/components/HomepageFeatures/features.tsx
@@ -1,97 +1,152 @@
 export type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
+  icon: string;
 };
 
 export const FeatureList: FeatureItem[] = [
     {
-      title: 'Easy to Use',
-      Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+      icon: 'ri-open-source-line',
+      title: 'Open-Source',
       description: (
         <>
-          Docusaurus was designed from the ground up to be easily installed and
-          used to get your website up and running quickly.
+          All components are open-source and self-hosted. 
+          No hidden services or paid extra content to unlock
         </>
       ),
     },
     {
-      title: 'Focus on What Matters',
-      Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+      icon: 'ri-hammer-line',
+      title: 'Buildpacks',
       description: (
         <>
-          Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-          ahead and move your docs into the <code>docs</code> directory.
+          A wide range of languages and frameworks are supported (PHP, Ruby, Golang, Rust, Nodejs, Deno)
         </>
       ),
     },
     {
-      title: 'Powered by React',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      icon: 'ri-git-pull-request-line',
+      title: 'Pull request Apps',
       description: (
         <>
-          Extend or customize your website layout by reusing React. Docusaurus can
-          be extended while reusing the same header and footer.
+          Deploy an app instance automaticly when a pull request is opened. Or deploy your app with a push to the Master
         </>
       ),
     },
     {
-      title: 'Powered by React',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      icon: 'ri-stack-line',
+      title: 'Addons',
       description: (
         <>
-          Extend or customize your website layout by reusing React. Docusaurus can
-          be extended while reusing the same header and footer.
+          Simple installation for addons like MySQL, Redis-Cluster, PostgreSQL, Kafka, CouchDB, Elasticsearch, MongoDB and more
         </>
       ),
     },
     {
-      title: 'Powered by React',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      icon: 'ri-time-line',
+      title: 'Cronjobs',
       description: (
         <>
-          Extend or customize your website layout by reusing React. Docusaurus can
-          be extended while reusing the same header and footer.
+          Schedule periodically running jobs to run at fixed times, dates, or intervals
         </>
       ),
     },
     {
-      title: 'Powered by React',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      icon: 'ri-line-chart-line',
+      title: 'Autoscale',
       description: (
         <>
-          Extend or customize your website layout by reusing React. Docusaurus can
-          be extended while reusing the same header and footer.
+          Scale horizontally and vertically to fit the size your application really needs
         </>
       ),
     },
     {
-      title: 'Powered by React',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      icon: 'ri-terminal-box-line',
+      title: 'CLI',
       description: (
         <>
-          Extend or customize your website layout by reusing React. Docusaurus can
-          be extended while reusing the same header and footer.
+          Use the CLI to create a Kubernetes cluster, to install Kubero and manage your applications
         </>
       ),
     },
     {
-      title: 'Powered by React',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      icon: 'ri-git-branch-line',
+      title: 'GitOps',
       description: (
         <>
-          Extend or customize your website layout by reusing React. Docusaurus can
-          be extended while reusing the same header and footer.
+          Connect your Pipeline with one of the many supported Git repositories (Github, Bitbucket, Gitea, Gitlab, Gogs ... )
         </>
       ),
     },
     {
-      title: 'Powered by React',
-      Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+      icon: 'ri-bug-line',
+      title: 'Vulnerability scans',
       description: (
         <>
-          Extend or customize your website layout by reusing React. Docusaurus can
-          be extended while reusing the same header and footer.
+          Run scans with trivy to detect Vulnerabilities in your images and repositories
+        </>
+      ),
+    },
+    {
+      icon: 'ri-paint-brush-line',
+      title: '130+ Templates',
+      description: (
+        <>
+          Use templates to quickly spin up well known tools like Wordpress or Kuma.
+        </>
+      ),
+    },
+    {
+      icon: 'ri-lock-line',
+      title: 'SSO Authentication',
+      description: (
+        <>
+          Choose from built-in, github or Oauth2 authentication method.
+        </>
+      ),
+    },
+    {
+      icon: 'ri-image-line',
+      title: 'CI/CD',
+      description: (
+        <>
+          Use the built-in CI/CD pipeline and registry to build your image from a Dockerfile or by Nixpacks.
+        </>
+      ),
+    },
+    {
+      icon: 'ri-zzz-line',
+      title: 'Sleeping container',
+      description: (
+        <>
+          Pods are turned off when not in use to save resources. When they are needed again, they can be started within milliseconds
+        </>
+      ),
+    },
+    {
+      icon: 'ri-window-line',
+      title: 'Webconsole',
+      description: (
+        <>
+          A console that can be used via the browser to maintain and debug the applications.
+        </>
+      ),
+    },
+    {
+      icon: 'ri-chat-1-line',
+      title: 'Notifications',
+      description: (
+        <>
+          Customizable notification of incidents on the installed apps.
+        </>
+      ),
+    },
+    {
+      icon: 'ri-heart-pulse-line',
+      title: 'Metrics and Monitoring',
+      description: (
+        <>
+          Predefined metrics and monitoring at a glance for the developer.
         </>
       ),
     },
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,26 +2,28 @@ import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
 import { FeatureItem, FeatureList } from './features';
+import 'remixicon/fonts/remixicon.css'
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({title, icon, description}: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
+    <div className="bg-slate-100 rounded-md drop-shadow-md transition duration-300 ease-in-out hover:scale-105">
+      <div className="px-4">
+        <i className={icon + " "+styles.icon}></i>
         <Heading as="h3">{title}</Heading>
+      </div>
+      <div className="px-4">
         <p>{description}</p>
       </div>
     </div>
   );
 }
 
+
 export default function HomepageFeatures(): JSX.Element {
   return (
     <section className={styles.features}>
       <div className="container">
-        <div className="row">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
