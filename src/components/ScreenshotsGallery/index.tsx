5c169ba67ea2eb14bd4a54ebbbc51f2e0ca8823b
Gianni Carafa <gianni.carafa@srf.ch>
2024-12-27 15:43:49 +0100

    fill screenshot callery

--- src/components/ScreenshotsGallery/index.tsx
+++ src/components/ScreenshotsGallery/index.tsx
@@ -3,10 +3,12 @@ import "yet-another-react-lightbox/styles.css";
 import Lightbox from "yet-another-react-lightbox";
 import { useState } from "react";
 import { images, CustomImage } from "./images";
+import styles from "yet-another-react-lightbox/styles.css";
+import stylesPlus from './styles.module.css';
 
 
-const slides = images.map(({ original, width, height }) => ({
-    src: original,
+const slides = images.map(({ src, width, height }) => ({
+    src: src,
     width: width * 4,
     height: height * 4,
   }));
@@ -17,11 +19,14 @@ export default function ScreenshotsGallery(): JSX.Element {
     const handleClick = (index: number, item: CustomImage) => setIndex(index);
 
     return (
-          <div>
+          <div className={stylesPlus.screenshotsGallery}>
             <GridGallery 
-                images={images} 
+                images={images}
+                rowHeight={400}
+                margin={10}
                 onClick={handleClick}
-                enableImageSelection={false}/>
+                enableImageSelection={false}
+                />
 
             <Lightbox
                 slides={slides}
