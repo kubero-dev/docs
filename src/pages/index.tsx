c631563d8462ba3746297b71ebe983c9ec101b5c
Gianni Carafa <gianni.carafa@srf.ch>
2024-12-27 11:59:48 +0100

    add grid gallery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,35 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
+import { Gallery as GridGallery } from "react-grid-gallery";
 
 import styles from './index.module.css';
+
+const images = [
+  {
+     src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+     width: 320,
+     height: 174,
+     isSelected: true,
+     caption: "After Rain (Jeshu John - designerspics.com)",
+  },
+  {
+     src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+     width: 320,
+     height: 212,
+     tags: [
+        { value: "Ocean", title: "Ocean" },
+        { value: "People", title: "People" },
+     ],
+     alt: "Boats (Jeshu John - designerspics.com)",
+  },
+  {
+     src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+     width: 320,
+     height: 212,
+  },
+];
+
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -45,6 +72,8 @@ export default function Home(): JSX.Element {
       <main>
         <HomepageFeatures />
       </main>
+      <hr />
+      <GridGallery images={images} />
     </Layout>
   );
 }
