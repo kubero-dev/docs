142befcb6a800b53b069fbaaa9a72b260918ef94
Gianni Carafa <gianni.carafa@srf.ch>
2025-01-02 14:44:09 +0100

    Update image dimensions and add new screenshots for different views in the ScreenshotsGallery component.

--- src/components/ScreenshotsGallery/images.ts
+++ src/components/ScreenshotsGallery/images.ts
@@ -8,8 +8,8 @@ export const images: CustomImage[] = [
   {
     src: "/assets/screenshots/app-overview.png",
     original: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-    width: 1528,
-    height: 2876,
+    width: 1185,
+    height: 1170,
     tags: [
       { value: "ui", title: "UI" },
     ],
@@ -18,8 +18,8 @@ export const images: CustomImage[] = [
   {
     src: "/assets/screenshots/pipeline-list.png",
     original: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-    width: 1300,
-    height: 1788,
+    width: 1179,
+    height: 1181,
     tags: [
       { value: "ui", title: "UI" },
     ],
@@ -28,8 +28,8 @@ export const images: CustomImage[] = [
   {
     src: "/assets/screenshots/app-metrics.png",
     original: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
-    width: 1409,
-    height: 931,
+    width: 1361,
+    height: 1821,
     tags: [
       { value: "ui", title: "UI" },
     ],
@@ -48,8 +48,8 @@ export const images: CustomImage[] = [
   {
     src: "/assets/screenshots/app-logs.png",
     original: "https://c6.staticflickr.com/9/8890/28897154101_a8f55be225_b.jpg",
-    width: 2688,
-    height: 1572,
+    width: 1181,
+    height: 1187,
     tags: [
       { value: "ui", title: "UI" },
     ],
@@ -58,7 +58,7 @@ export const images: CustomImage[] = [
   {
     src: "/assets/screenshots/app-vulnerabilities.png",
     original: "https://c5.staticflickr.com/9/8768/28941110956_b05ab588c1_b.jpg",
-    width: 1084,
+    width: 1184,
     height: 1485,
     tags: [
       { value: "ui", title: "UI" },
@@ -68,8 +68,8 @@ export const images: CustomImage[] = [
   {
     src: "/assets/screenshots/pipeline-list.png",
     original: "https://c3.staticflickr.com/9/8583/28354353794_9f2d08d8c0_b.jpg",
-    width: 2952,
-    height: 1456,
+    width: 1179,
+    height: 1179,
     tags: [
       { value: "ui", title: "UI" },
     ],
@@ -78,8 +78,8 @@ export const images: CustomImage[] = [
   {
     src: "/assets/screenshots/app-new-addon.png",
     original: "https://c7.staticflickr.com/9/8569/28941134686_d57273d933_b.jpg",
-    width: 1219,
-    height: 1140,
+    width: 1182,
+    height: 1183,
     tags: [
       { value: "ui", title: "UI" },
     ],
@@ -88,8 +88,8 @@ export const images: CustomImage[] = [
   {
     src: "/assets/screenshots/cli.png",
     original: "https://c6.staticflickr.com/9/8342/28897193381_800db6419e_b.jpg",
-    width: 694,
-    height: 441,
+    width: 753,
+    height: 639,
     tags: [
       { value: "cli", title: "CLI" },
     ],
