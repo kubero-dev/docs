6a58553736a26c98013d9972e038e6c44d5a1bb1
Gianni Carafa <gianni.carafa@srf.ch>
2025-01-15 21:34:15 +0100

    add some new features

--- src/components/HomepageFeatures/features.tsx
+++ src/components/HomepageFeatures/features.tsx
@@ -28,16 +28,6 @@ export const FeatureList: FeatureItem[] = [
       ),
     },
     {
-      icon: 'ri-paint-brush-line',
-      title: '140+ Templates',
-      link: '/docs/Usermanual/features#templates',
-      description: (
-        <>
-          Use templates for one-click-deplyoments of well known tools like Wordpress or Kuma.
-        </>
-      ),
-    },
-    {
       icon: 'ri-apps-2-ai-line',
       title: 'High Availability',
       link: '/docs/Usermanual/features#high-availability',
@@ -48,8 +38,18 @@ export const FeatureList: FeatureItem[] = [
       ),
     },
     {
+      icon: 'ri-paint-brush-line',
+      title: '140+ Templates',
+      link: '/docs/Usermanual/features#templates',
+      description: (
+        <>
+          Use templates for one-click-deplyoments of well known tools like Wordpress or Kuma.
+        </>
+      ),
+    },
+    {
       icon: 'ri-stack-line',
-      title: 'Addons',
+      title: 'Add-ons',
       link: '/docs/Usermanual/features#addons',
       description: (
         <>
@@ -63,7 +63,7 @@ export const FeatureList: FeatureItem[] = [
       link: '/docs/Usermanual/features#metrics-and-monitoring',
       description: (
         <>
-          Predefined metrics and monitoring at a glance for the developer.
+          Predefined application metrics and monitoring at a glance for the developer.
         </>
       ),
     },
@@ -73,7 +73,7 @@ export const FeatureList: FeatureItem[] = [
       link: '/docs/Usermanual/features#cronjobs',
       description: (
         <>
-          Schedule periodically running jobs to run at fixed times, dates, or intervals
+          Schedule periodically running jobs to run at fixed times, dates, or intervals.
         </>
       ),
     },
@@ -83,17 +83,17 @@ export const FeatureList: FeatureItem[] = [
       link: '/docs/Usermanual/features#webconsole',
       description: (
         <>
-          Access your pods and containers directly from the brower with the integrated webconsole
+          Access your pods and containers directly from the brower with the integrated webconsole.
         </>
       ),
     },
     {
       icon: 'ri-git-branch-line',
-      title: 'Push to deploy',
+      title: 'Push to Deploy',
       link: '/docs/Usermanual/features#gitops',
       description: (
         <>
-          Connect your Pipeline with your Git repository to build and deploy your application (GitOps)
+          Connect your Pipeline with your Git repository to build and deploy your application (GitOps).
         </>
       ),
     },
@@ -103,13 +103,13 @@ export const FeatureList: FeatureItem[] = [
       link: '/docs/Usermanual/features#buildpacks',
       description: (
         <>
-          Use buildpacks.io, nixpacks or runpacks to build your application without writing a Dockerfile
+          Use a Dockerfile, <b><a href="https://buildpacks.io/" target="_blank">buildpacks.io</a></b>, <b><a href="https://nixpacks.com/docs/getting-started" target="_blank">nixpacks</a></b> or runpacks to build your application without writing a Dockerfile
         </>
       ),
     },
     {
       icon: 'ri-git-pull-request-line',
-      title: 'Pull request Apps',
+      title: 'Pull Request Apps',
       link: '/docs/Usermanual/features#pull-requtst-apps',
       description: (
         <>
@@ -119,17 +119,37 @@ export const FeatureList: FeatureItem[] = [
     },
     {
       icon: 'ri-bug-line',
-      title: 'Vulnerability scans',
+      title: 'Vulnerability Scans',
       link: '/docs/Usermanual/features#vulnerability-scans',
       description: (
         <>
-          Run scans with trivy to detect Vulnerabilities in your images and repositories
+          Run scans with trivy to detect Vulnerabilities in your running images and repositories.
         </>
       ),
     },
     {
       icon: 'ri-lock-line',
-      title: 'Oauth2 support',
+      title: 'Basic Authentication',
+      link: '/docs/Usermanual/features#oauth2-authentication',
+      description: (
+        <>
+          Protect your applications with basic authentication and manage users and passwords.
+        </>
+      ),
+    },
+    {
+      icon: 'ri-shield-check-line',
+      title: 'SSL Certs',
+      link: '/docs/Usermanual/features#oauth2-authentication',
+      description: (
+        <>
+          Issue SSL certificates for your applications with cert-manager and Let's Encrypt.
+        </>
+      ),
+    },
+    {
+      icon: 'ri-login-circle-line',
+      title: 'Oauth2 Support',
       link: '/docs/Usermanual/features#oauth2-authentication',
       description: (
         <>
@@ -139,11 +159,11 @@ export const FeatureList: FeatureItem[] = [
     },
     {
       icon: 'ri-zzz-line',
-      title: 'Sleeping container',
+      title: 'Sleeping Containers',
       link: '/docs/Usermanual/features#sleeping-contianers',
       description: (
         <>
-          Pods are turned off when not in use to save resources. When they are needed again, they can be started within milliseconds
+          Pods are turned off when not in use to save resources. When they are needed again, they can be started within milliseconds.
         </>
       ),
     },
@@ -154,6 +174,26 @@ export const FeatureList: FeatureItem[] = [
       description: (
         <>
           Use the CLI to create a Kubernetes cluster, to install Kubero and manage your applications and pipelines
+        </>
+      ),
+    },
+    {
+      icon: 'ri-braces-line',
+      title: 'API',
+      link: '/docs/Usermanual/features#cli',
+      description: (
+        <>
+          Use the API to extend your own applications with Kubero's features.
+        </>
+      ),
+    },
+    {
+      icon: 'ri-code-box-line',
+      title: 'IaC',
+      link: '/docs/Usermanual/features#cli',
+      description: (
+        <>
+          Deploy Kubero's Pipelines and Applications with your own CI/CD pipeline.
         </>
       ),
     },
